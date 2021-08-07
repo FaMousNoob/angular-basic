36fb34be0789c909dfdf1dcaa19d5672a64d1c71
P.Tao <ngobaophuongtao@gmail.com>
2021-08-07 16:15:22 +0700

    new lessons

--- src/app/directives/structural/structural.component.ts
+++ src/app/directives/structural/structural.component.ts
@@ -8,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class StructuralComponent implements OnInit {
   isShow: boolean = true;
   output: string = '';
+  replaceText: string = 'this is a replace text';
+
+  month: number = 1;
+  trueColor: string = 'bg-danger';
 
   switching() {
     this.isShow = !this.isShow;
+  }
+
+  changeMonth() {
+    if (this.month < 4) {
+      this.month += 1;
+    } else {
+      this.month = 1;
+    }
+  }
+
+  handleColor(color: string) {
+    switch (color) {
+      case 'red':
+        this.trueColor = 'bg-danger';
+        break;
+      case 'blue':
+        this.trueColor = 'bg-primary';
+        break;
+      case 'green':
+        this.trueColor = 'bg-success';
+        break;
+    }
   }
 
   constructor() {}
